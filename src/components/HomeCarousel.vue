c1949f8835e10e23b4d47d66733171cc8cfa57ef
Saimen-Luo <31783769+Saimen-Luo@users.noreply.github.com>
2021-07-26 17:15:15 +0800

    feat: add HomeCarousel component

---
+++ src/components/HomeCarousel.vue
@@ -0,0 +1,109 @@
+<template>
+  <div class="slide-wrapper" ref="slide">
+    <div class="slide-content">
+      <img
+        v-for="swiper in swiperList"
+        :src="swiper.imgUrl"
+        :key="swiper.id"
+        alt="swiper-img"
+        class="slide-page"
+      />
+    </div>
+    <div class="dots-wrapper">
+      <span
+        class="dot"
+        v-for="num in swiperList.length"
+        :key="num"
+        :class="{ active: currentPageIndex === num - 1 }"
+      ></span>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, onMounted, ref } from "vue";
+import BScroll from "@better-scroll/core";
+import Slide from "@better-scroll/slide";
+
+BScroll.use(Slide);
+
+interface page {
+  pageX: number;
+}
+
+const swiperList = [
+  {
+    id: "0001",
+    imgUrl:
+      "http://img1.qunarzz.com/piao/fusion/1801/1a/94428c6dea109402.jpg_640x200_2cf590d8.jpg",
+  },
+  {
+    id: "0002",
+    imgUrl:
+      "http://img1.qunarzz.com/piao/fusion/1802/42/7c92b9a381e46402.jpg_640x200_1cdce2a4.jpg",
+  },
+];
+
+export default defineComponent({
+  setup() {
+    const slide = ref();
+    const currentPageIndex = ref(0);
+    onMounted(() => {
+      const scroll = new BScroll(slide.value, {
+        scrollX: true,
+        scrollY: false,
+        slide: {
+          loop: true,
+          autoplay: false,
+        },
+        momentum: false,
+        bounce: false,
+        probeType: 3,
+      });
+      scroll.on("slideWillChange", (page: page) => {
+        currentPageIndex.value = page.pageX;
+      });
+    });
+    return {
+      swiperList,
+      slide,
+      currentPageIndex,
+    };
+  },
+});
+</script>
+
+<style lang="stylus">
+.slide-wrapper {
+  position: relative;
+  overflow: hidden;
+  width: 100%;
+  height: 0;
+  padding-bottom: 31.25%;
+  background: #eee;
+
+  .slide-page {
+    width: 100%;
+  }
+
+  .dots-wrapper {
+    position: absolute;
+    bottom: 0.08rem;
+    left: 50%;
+    transform: translateX(-50%);
+
+    .dot {
+      display: inline-block;
+      margin: 0 0.08rem;
+      width: 0.16rem;
+      height: 0.16rem;
+      border-radius: 50%;
+      background: #eee;
+
+      &.active {
+        background-color: #fff;
+      }
+    }
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,20 @@
 <template>
   <div>
     <home-header />
+    <home-carousel />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import HomeHeader from "../components/HomeHeader.vue";
+import HomeCarousel from "../components/HomeCarousel.vue";
 
 export default defineComponent({
   name: "Home",
   components: {
     HomeHeader,
+    HomeCarousel,
   },
 });
 </script>
