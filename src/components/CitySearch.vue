9231d61b5e2cfde2beeaccaf1c381812c3e727cc
Saimen-Luo <31783769+Saimen-Luo@users.noreply.github.com>
2021-08-03 10:51:19 +0800

    feat: add CitySearch component

---
+++ src/components/CitySearch.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="search">
+    <input class="search__input" type="text" placeholder="输入城市名或拼音" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "CitySearch",
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/variables.styl';
+
+.search {
+  height: 0.72rem;
+  padding: 0 0.1rem;
+  background: $bgColor;
+
+  .search__input {
+    box-sizing: border-box;
+    width: 100%;
+    height: 0.62rem;
+    padding: 0 0.1rem;
+    line-height: 0.62rem;
+    text-align: center;
+    border-radius: 0.06rem;
+    color: #666;
+  }
+}
+</style>
--- src/views/City.vue
+++ src/views/City.vue
@@ -1,14 +1,19 @@
 <template>
   <city-header />
+  <city-search />
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import CityHeader from "@/components/CityHeader.vue";
+import CitySearch from "@/components/CitySearch.vue";
 
 export default defineComponent({
   name: "City",
-  components: { CityHeader },
+  components: {
+    CityHeader,
+    CitySearch,
+  },
   setup() {
     return {};
   },
