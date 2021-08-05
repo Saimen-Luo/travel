d910e1474156cb381d7fb1f32fa1501d6af95006
Saimen-Luo <31783769+Saimen-Luo@users.noreply.github.com>
2021-08-05 11:01:49 +0800

    perf: get startY only once, throttle touchmove

--- src/components/CityAlphabet.vue
+++ src/components/CityAlphabet.vue
@@ -21,7 +21,7 @@
 </template>
 
 <script lang="ts">
-import { computed, defineComponent, PropType, ref } from "vue";
+import { computed, defineComponent, watch, nextTick, PropType, ref } from "vue";
 import { useStore } from "vuex";
 
 import { AlphabetCities } from "@/common/interfaces";
@@ -38,8 +38,17 @@ export default defineComponent({
   setup(props) {
     const currentIndex = ref<number>();
     const alphabets = ref<{ [key: string]: HTMLElement }>({});
+    const startY = ref(0);
+    const timer = ref();
     const letters = computed(() => Object.keys(props.cities));
     const store = useStore<GlobalState>();
+    watch(
+      () => letters.value,
+      async () => {
+        await nextTick();
+        startY.value = alphabets.value["A"].offsetTop;
+      }
+    );
     const handleLetterClick = (e: MouseEvent) => {
       const target = e.target as HTMLElement;
       // console.log(target.innerText);
@@ -47,25 +56,31 @@ export default defineComponent({
       currentIndex.value = letters.value.indexOf(target.innerText);
     };
     const handleTouchMove = (e: TouchEvent) => {
-      // 只有 touchmove 事件e.touches有值
-      // console.log("touchMove", e.touches);
-      // rem = 50px
-      // .header 0.86rem + .search 0.72rem
-      // offsetTop：document 属性，相当于父元素
-      // clientY 事件对象属性，相对于 视口
-      const startY = alphabets.value["A"].offsetTop;
-      const rootFontSize = Number.parseInt(
-        document.documentElement.style.fontSize
-      );
-      // console.log(rootFontSize);
-      const touchY = e.touches[0].clientY - (0.86 + 0.72) * rootFontSize;
-      // .item 0.4 rem
-      currentIndex.value = Math.floor((touchY - startY) / (0.4 * rootFontSize));
-      const letter = letters.value[currentIndex.value];
-      // console.log(letter);
-      if (letter) {
-        store.commit("changeLetter", letter);
+      if (timer.value) {
+        clearTimeout(timer.value);
       }
+      timer.value = setTimeout(() => {
+        // 只有 touchmove 事件e.touches有值
+        // console.log("touchMove", e.touches);
+        // rem = 50px
+        // .header 0.86rem + .search 0.72rem
+        // offsetTop：document 属性，相当于父元素
+        // clientY 事件对象属性，相对于 视口
+        const rootFontSize = Number.parseInt(
+          document.documentElement.style.fontSize
+        );
+        // console.log(rootFontSize);
+        const touchY = e.touches[0].clientY - (0.86 + 0.72) * rootFontSize;
+        // .item 0.4 rem
+        currentIndex.value = Math.floor(
+          (touchY - startY.value) / (0.4 * rootFontSize)
+        );
+        const letter = letters.value[currentIndex.value];
+        // console.log(letter);
+        if (letter) {
+          store.commit("changeLetter", letter);
+        }
+      }, 20);
     };
     return {
       letters,
