7e7e4b73bc8c1d990d69504f84fe5826513b3742
Saimen-Luo <31783769+Saimen-Luo@users.noreply.github.com>
2021-08-05 21:14:20 +0800

    feat: save city value to localStorage

--- src/store/index.ts
+++ src/store/index.ts
@@ -5,10 +5,19 @@ export interface GlobalState {
   city: string;
 }
 
+let city = "北京";
+try {
+  if (localStorage.city) {
+    city = localStorage.city;
+  }
+} catch (error) {
+  console.log(error);
+}
+
 export default createStore<GlobalState>({
   state: {
     clickedLetter: "",
-    city: "北京",
+    city,
   },
   mutations: {
     changeLetter(state, letter) {
@@ -16,6 +25,11 @@ export default createStore<GlobalState>({
     },
     changeCity(state, city) {
       state.city = city;
+      try {
+        localStorage.city = city;
+      } catch (error) {
+        console.log(error);
+      }
     },
   },
   actions: {},
